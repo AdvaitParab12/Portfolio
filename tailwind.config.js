dfa8df23ebd84c47f3ff68c1741f91c69ee62969
advait0012 <advait0012@proton.me>
2024-11-19 02:59:41 +0530

    learn more page half done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,html}"],
   theme: {
     fontFamily: {
       sans: ["Montserrat", "monospace"],
+    },
+    fontFamily:{
+      serif:["Poppins","monospace"]
     },
     extend: {
       colors: {
